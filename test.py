b41d8a8daf58a877de9f5ab73728ece7bbba509b
骆轩源 <luoxy15@mails.tsinghua.edu.cn>
2018-04-29 14:34:26 +0800

    dfsdf

--- test.py
+++ test.py
@@ -10,8 +10,8 @@ for f1 in fs:
 	if 'trace.gz' in tmp_path:
 		tmp_id = tmp_path[2:5] + '.stats'
 		tmp_path = '.' + tmp_path
-		sh += '../bin/CMPsim.usertrace.64 -threads 1 -t '+ tmp_path + ' -o ' + tmp_id + '0 -cache UL3:1024:64:16 -LLCrepl 0\n'
-		sh += '../bin/CMPsim.usertrace.64 -threads 1 -t '+ tmp_path + ' -o ' + tmp_id + '1 -cache UL3:1024:64:16 -LLCrepl 1\n'
-		sh += '../bin/CMPsim.usertrace.64 -threads 1 -t '+ tmp_path + ' -o ' + tmp_id + '2 -cache UL3:1024:64:16 -LLCrepl 2\n'
+		sh += '../bin/CMPsim.usetrace.64 -threads 1 -t '+ tmp_path + ' -o ' + tmp_id + '0 -cache UL3:1024:64:16 -LLCrepl 0\n'
+		sh += '../bin/CMPsim.usetrace.64 -threads 1 -t '+ tmp_path + ' -o ' + tmp_id + '1 -cache UL3:1024:64:16 -LLCrepl 1\n'
+		sh += '../bin/CMPsim.usetrace.64 -threads 1 -t '+ tmp_path + ' -o ' + tmp_id + '2 -cache UL3:1024:64:16 -LLCrepl 2\n'
 		break
 open('runs/do_run.sh','w').write(sh)
